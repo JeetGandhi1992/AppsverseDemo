82f4da74744379539b17ab4861ac392e9a1a9bdc
Jeet Gandhi <jeet_gandhi@singaporeair.com.sg>
2020-12-13 03:21:58 +0800

    Adding pods

---
+++ Pods/Target Support Files/Pods-AppsverseDemo-AppsverseDemoUITests/Pods-AppsverseDemo-AppsverseDemoUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AppsverseDemo_AppsverseDemoUITests : NSObject
+@end
+@implementation PodsDummy_Pods_AppsverseDemo_AppsverseDemoUITests
+@end
---
+++ Pods/Target Support Files/Pods-AppsverseDemo/Pods-AppsverseDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AppsverseDemo : NSObject
+@end
+@implementation PodsDummy_Pods_AppsverseDemo
+@end
